7f5e32d5999e7e303d738ba83e954adfc4502193
connorsnelgrove28 <ConnorSnelgrove28@gmail.com>
2026-02-04 21:06:21 -0500

    Adding Pause to gameplay by hitting P

--- src/screens/play.py
+++ src/screens/play.py
@@ -6,8 +6,14 @@ class PlayScreen(ScreenBase):
         self.app = app
         self.game = Game(Player())
         cavern.game = self.game
+        self.paused = False
 
     def update(self, input_state):
+        if input_state and input_state.pause_pressed:
+            self.paused = not self.paused
+            return
+        if self.paused:
+            return
         if self.game.player.lives < 0:
             self.game.play_sound("over")
             from .game_over import GameOverScreen
@@ -18,3 +24,5 @@ class PlayScreen(ScreenBase):
     def draw(self):
         self.game.draw()
         cavern.draw_status()
+        if self.paused:
+            cavern.screen.draw.text("PAUSED", center=(cavern.WIDTH // 2, cavern.HEIGHT // 2), fontsize=64, color="white",)
